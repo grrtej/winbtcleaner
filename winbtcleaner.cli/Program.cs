f4e7c0d9eb386bdd8bac7419307c1f9ae0f95c2e
Gurtej Saini <grrtej@gmail.com>
2025-10-24 18:08:31 -0500

    create a cli interface

---
+++ winbtcleaner.cli/Program.cs
@@ -0,0 +1,11 @@
+﻿using winbtcleaner.core;
+
+IDeviceManager deviceManager = new MockDeviceManager();
+
+await deviceManager.RefreshDevices();
+
+Console.WriteLine("Paired Microsoft Bluetooth Keyboards:");
+foreach (var device in deviceManager.Devices)
+{
+    Console.WriteLine($"- {device.Name}");
+}
