0741ed4c40c43a1fa4d68362fc759351a2a29d7d
Gurtej Saini <grrtej@gmail.com>
2025-10-24 18:08:14 -0500

    create mock device and device manager classes

---
+++ winbtcleaner.core/MockDevice.cs
@@ -0,0 +1,11 @@
+﻿namespace winbtcleaner.core;
+
+public class MockDevice : IDevice
+{
+    public MockDevice(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+}
---
+++ winbtcleaner.core/MockDeviceManager.cs
@@ -0,0 +1,21 @@
+﻿namespace winbtcleaner.core;
+
+public class MockDeviceManager : IDeviceManager
+{
+    private readonly List<MockDevice> _devices = new();
+
+    public async Task RefreshDevices()
+    {
+        _devices.Clear();
+        var random = new Random();
+        int deviceCount = random.Next(5, 16);
+        for (int i = 0; i < deviceCount; i++)
+        {
+            int delay = random.Next(50, 201);
+            await Task.Delay(delay);
+            _devices.Add(new MockDevice($"Mock Device {i + 1}"));
+        }
+    }
+
+    public IEnumerable<IDevice> Devices => _devices;
+}
